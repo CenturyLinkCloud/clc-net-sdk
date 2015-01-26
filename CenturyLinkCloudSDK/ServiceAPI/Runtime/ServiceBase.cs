f4d4237c2f54f55de808e3e79e5d7ed849ac1799
Mario Mamalis <mario.mamalis@gmail.com>
2015-01-26 13:10:37 -0600

    Changed some comments

--- CenturyLinkCloudSDK/ServiceAPI/Runtime/ServiceBase.cs
+++ CenturyLinkCloudSDK/ServiceAPI/Runtime/ServiceBase.cs
@@ -20,7 +20,7 @@ namespace CenturyLinkCloudSDK.ServiceAPI.Runtime
         /// <typeparam name="TRequest"></typeparam>
         /// <typeparam name="TResponse"></typeparam>
         /// <param name="request"></param>
-        /// <returns>An asynchronous Task of the generic TResponse which must implement IServiceRequest</returns>
+        /// <returns>An asynchronous Task of the generic TResponse which must implement IServiceResponse</returns>
         internal async Task<TResponse> Invoke<TRequest, TResponse>(TRequest request) 
             where TRequest : ServiceRequest 
             where TResponse : IServiceResponse
--- CenturyLinkCloudSDK/ServiceAPI/Runtime/ServiceRequestModel.cs
+++ CenturyLinkCloudSDK/ServiceAPI/Runtime/ServiceRequestModel.cs
@@ -1,7 +1,7 @@
 ﻿namespace CenturyLinkCloudSDK.ServiceAPI.Runtime
 {
     /// <summary>
-    /// This base class inherited by all service request models provides an array that can store request parameters.
+    /// This base class inherited by all service request models provides an array that can store request body data.
     /// The purpose of this array is to be used by requests that do not need to be serialized from specific types,
     /// but rather contain an unnamed array of data. The UnNamedArray property is used in the Invoke method,
     /// of the ServiceAPI base class, in order to determine the type of serialization to perform.
