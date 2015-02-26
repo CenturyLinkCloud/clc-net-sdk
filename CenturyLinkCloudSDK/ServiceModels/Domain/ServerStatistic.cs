01635f832e90ec35060c23fb50a29e30210cf076
Mario Mamalis <mario.mamalis@gmail.com>
2015-02-26 15:47:42 -0600

    Re-added a domain model class because somehow it was not in source control and it was causing an error on the Jenkins build.

---
+++ CenturyLinkCloudSDK/ServiceModels/Domain/ServerStatistic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenturyLinkCloudSDK.ServiceModels
+{
+    public class ServerStatistic
+    {
+        public DateTime Timestamp { get; set; }
+
+        public short Cpu { get; set; }
+
+        public decimal CpuPercent { get; set; }
+
+        public int MemoryMB { get; set; }
+
+        public decimal MemoryPercent { get; set; }
+
+        public long NetworkReceivedKBps { get; set; }
+
+        public long NetworkTransmittedKBps { get; set; }
+
+        public long DiskUsageTotalCapacityMB { get; set; }
+
+        public IEnumerable<DiskUsageStatistic> DiskUsage { get; set; }
+
+        public IEnumerable<GuestDiskUsageStatistic> GuestDiskUsage { get; set; }
+    }
+}
