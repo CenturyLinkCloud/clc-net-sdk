76596a5642bf6fec7dc2280ea769f9de7b26377c
Rob Pierry <rpierry@capturaonline.com>
2015-07-07 08:32:14 -0700

    move credentials to separate file

---
+++ CenturyLinkCloudSDK.Integration.Tests/Credentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenturyLinkCloudSDK.IntegrationTests
+{
+    public static class Credentials
+    {
+        public static string Username = "";
+        public static string Password = "";
+    }
+}
